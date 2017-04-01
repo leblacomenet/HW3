dfc50786702037b5213f919e4d9c4715fb09bed7
Sebastien Penet <contact@sebastienpenet.com>
2017-04-01 16:25:36 +0200

    Update hw3.r

--- hw3.r
+++ hw3.r
@@ -2,9 +2,9 @@ setwd("/Users/sebastienpenet/Documents/ESSEC/Forecasting/HW3-master")
 
 # LOAD
 
-gas = read.csv('data/gas.csv')
-temp = read.csv('data/temp.csv')
-google = read.csv('data/google.csv')
+gas = na.omit(read.csv('data/gas.csv'))
+temp = na.omit(read.csv('data/temp.csv'))
+google = na.omit(read.csv('data/google.csv'))
 
 summary(gas)
 gas$date <- as.Date(gas$date)
