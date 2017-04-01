1e125890c352469c175b2a857691a674c0ce4522
Sebastien Penet <contact@sebastienpenet.com>
2017-04-01 16:20:02 +0200

    Update hw3.r

--- hw3.r
+++ hw3.r
@@ -1,3 +1,4 @@
+setwd("/Users/sebastienpenet/Documents/ESSEC/Forecasting/HW3-master")
 
 # LOAD
 
@@ -5,3 +6,7 @@ gas = read.csv('data/gas.csv')
 temp = read.csv('data/temp.csv')
 google = read.csv('data/google.csv')
 
+summary(gas)
+gas$date <- as.Date(gas$date)
+plot(gas$date, gas$gas_cons, type="l")
+plot(gas$date, gas$log_gas_cons, type="l")
