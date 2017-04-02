ce96a5db05a7598f0d8b76224f0fa23d0b14c89e
hippolytej <hippolyte.jacomet@gmail.com>
2017-04-02 09:55:45 +0200

    wake up commit

--- hw3.r
+++ hw3.r
@@ -1,25 +1,33 @@
-setwd("/Users/sebastienpenet/Documents/ESSEC/Forecasting/HW3-master")
+
+# HOMEWORK 3 - FORECASTING MODELS
+
+# Environment
 library(forecast)
 
-# LOAD
-
+# Data
 gas <- na.omit(read.csv('data/gas.csv'))
-temp <- na.omit(read.csv('data/temp.csv'))
-google <- na.omit(read.csv('data/google.csv'))
+temp <- read.csv('data/temp.csv')
+google <- read.csv('data/google.csv')
 
+# Pre-process dates
 temp$date <- lapply(temp$date, function(x) { as.Date(format(as.Date(x), '%Y-%m-01')) })
 gas$date <- lapply(gas$date, function(x) { as.Date(format(as.Date(x), '%Y-%m-01')) })
 google$date <- lapply(google$date, function(x) { as.Date(format(as.Date(x), '%Y-%m-01')) })
 
+# Create and plot Time-Series Objects
 gasc <- ts(gas$gas_cons[1:510], frequency=12)
 gaslc <- ts(gas$log_gas_cons[1:510], frequency=12)
 plot(gasc)
 plot(gaslc)
 
+# Forecast after seasonal decomposition
+
+# Consumption
 gasc_d <- stl(gasc, t.window=12, s.window="periodic")
 gasc_d_fit <- forecast(gasc_d, h=12)
 plot(gasc_d_fit)
 
+# Log consumption
 gaslc_d <- stl(gaslc, t.window=12, s.window="periodic")
 gaslc_d_fit <- forecast(gaslc_d, h=12)
 plot(gaslc_d_fit)
